9a4e557e81b2c361a011744da4c56d3e4f5bdc2f
gzlgfang <lgfang@scut.edu.cn>
2024-09-06 19:51:07 +0800

    add

--- 第5章代码/02-1-Bisection.py
+++ 第5章代码/02-1-Bisection.py
@@ -1,8 +1,9 @@
-# 03-golden method
+# 二分法bisection method
+# 02-Bisection
 import scipy as scp
 import numpy as np
 import time
-
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
@@ -15,81 +16,52 @@ mpl.rcParams["font.size"] = 18  # 设置字体大小
 mpl.rcParams["font.style"] = "oblique"  # 设置字体风格，倾斜与否
 mpl.rcParams["font.weight"] = "normal"  # "normal",=500，设置字体粗细
 
-
 # 自定义优化函数
-def func(t):
-    return 225 * np.log(14 - 0.1 * t) / (130 - t) + 480 / (t - 30)
+# def func(t):
+# return 225 * np.log(14 - 0.1 * t) / (130 - t) + 480 / (t - 30)
 
 
-# 快速扫描法程序
-def fast_scan(a):
-    n, alfai = 1, 0.1
-    t1 = a
-    f1 = func(t1)
-    t2 = t1 + alfai * 2 ** (n - 1)
-    f2 = func(t2)
-    if f1 < f2:
-        alfai = -alfai
-        t2 = t1 + alfai * 2 ** (n - 1)
-    while n < 1000:
-        n = n + 1
-        t3 = t2 + alfai * 2 ** (n - 1)
-        f3 = func(t3)
-        if f2 < f3:
-            break
-        else:
-            t1 = t2
-            f1 = f2
-            t2 = t3
-            f2 = f3
-    return [t1, t3]
+def func(x):  ##fitness
+    a1, b1, c1, d1 = 2, 6, -5, 7
+    f = ((x + a1) ** 2 + b1**2) ** 0.5 + ((x + c1) ** 2 + d1**2) ** 0.5
+    return f
 
 
-Opt_inter = fast_scan(32)
-# print(Opt_inter)
-# 黄金分割法
 start_time = time.time()
-eer1 = 0.00000001
-eer2 = 0.00000001
-beita = 0.5 * (5**0.5 - 1)
-a = Opt_inter[0]
-b = Opt_inter[1]
-x1 = a + (1 - beita) * (b - a)
-x2 = b - (1 - beita) * (b - a)
-f1 = func(x1)
-f2 = func(x2)
+# t1, t2, n = 32, 128, 0
+# t1, t2, n = -5.5, -0.5, 0
+t1, t2, n = 1.1, 1.7, 0
+
+x_start, x_end = t1, t2
+a0, b0 = t1, t2
+beita = 0.005
+eer1 = 0.000001
+eer2 = 0.000001
 flag = True
-n = 0
-a0, b0 = a, b
 while flag:
     n = n + 1
+    a = t1
+    b = t2
+    x1 = (a + b) / 2 - beita * (b - a)
+    x2 = (a + b) / 2 + beita * (b - a)
+    f1 = func(x1)
+    f2 = func(x2)
     if f1 >= f2:
+        t1 = x1
         e1 = abs((b - x1) / (a0 - b0))
         optim = x2
         minf = f2
-        e2 = abs(f2 - f1) / (1 + abs(f1))
-        a = x1
-        x1 = x2
-        f1 = f2
-        x2 = b - (1 - beita) * (b - a)
-        f2 = func(x2)
     else:
+        t2 = x2
+        e1 = abs((b - x1) / (a0 - b0))
         optim = x1
         minf = f1
-        e1 = abs((x2 - a) / (a0 - b0))
-        e2 = abs(f2 - f1) / (1 + abs(f1))
-        b = x2
-        x2 = x1
-        x1 = a + (1 - beita) * (b - a)
-        f2 = f1
-        f1 = func(x1)
+    e2 = abs(f2 - f1) / (1 + abs(f1))
     if e1 <= eer1 and e2 <= eer2:
         flag = False
 print(f"t={optim:.8f},minf={minf:.8f},n={n}")
 end_time = time.time()
 print("程序运行计时", end_time - start_time)
-
-x_start, x_end = a0, b0
 x = np.linspace(x_start, x_end, 100)
 y = func(x)
 plt.figure(num="目标函数与优化区间变量关系图")
@@ -100,11 +72,11 @@ plt.ylabel("优化函数值")
 plt.annotate(
     "最优点",
     xy=(optim, minf),
-    xytext=(optim + 5, minf + 3.5),
+    xytext=(optim + 1, minf + 0.5),
     arrowprops=dict(arrowstyle="->", color="r", linewidth=2),
 )
 str1 = "x=" + str(optim)
 str2 = "最优值=" + str(minf)
-plt.text(optim - 0.5, minf + 2.5, str1)
-plt.text(optim - 0.5, minf + 1.5, str2)
+plt.text(optim - 0.5, minf + 0.25, str1)
+plt.text(optim - 0.5, minf + 0.15, str2)
 plt.show()
--- 第5章代码/02-2-Bisection.py
+++ 第5章代码/02-2-Bisection.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import time
 
 mpl.rcParams["xtick.direction"] = "in"  # 坐标轴上的短线朝内，默认朝外
 plt.rcParams["ytick.direction"] = "in"
@@ -11,43 +12,53 @@ mpl.rcParams["axes.unicode_minus"] = False  # 保证负号显示
 mpl.rcParams["font.size"] = 18  # 设置字体大小
 mpl.rcParams["font.style"] = "oblique"  # 设置字体风格，倾斜与否
 mpl.rcParams["font.weight"] = "normal"  # "normal",=500，设置字体粗细
+start_time = time.time()
 
 
-def Exhau(funJ, N, Pc, start, alfai):
+def Bisexhau(funJ, beita, eer1, eer2, start, alfai):
     """
-    funJ  优化函数
-    N     区间等分数
-    PC    收缩率，Percentage of contraction
-    alfai 扫描步长
-    start 扫描起点
+    funJ      优化函数
+    beita     区间等分数
+    eer1      变量精度
+    eer2      函数精度
+    alfai     扫描步长
+    start     扫描起点
     """
-    global a, b, c, d, e
-    a, b, c, d, e = 1, 3, 5, 7, 2
-    flag = True
-    temp, k = 1, 0
-    while flag:
-        temp = temp * 1 / N
-        k = k + 1
-        if temp <= Pc:
-            flag = False
+
     Opt_inter = fast_scan(start, alfai)
     x_start = Opt_inter[0]
     x_end = Opt_inter[1]
     t1, t2 = x_start, x_end
-    f = np.ones(N + 1)
-    for m in range(k):  # 进行k轮穷举
-        t = np.linspace(t1, t2, N + 1)
-        for i in range(N + 1):  # 每轮穷举N等分
-            f[i] = funJ(t[i], a, b, c, d, e)
-        fmin = min(f[:])  # 确定最小值
-        index = list(f[:]).index(min(f[:]))  # 确定最小值所在的位置
-        t1 = t[index - 1]  # 重新设置t1
-        t2 = t[index + 1]
-    optim = t[index]
-    minf = funJ(t[index], a, b, c, d, e)
-    print(f"t={optim:.8f},minf={minf:.8f}")
+    a0, b0 = x_start, x_end
+    flag = True
+    print("t1,t2=", t1, t2)
+    n = 0
+    while flag:
+        n = n + 1
+        a = t1
+        b = t2
+        x1 = (a + b) / 2 - beita * (b - a)
+        x2 = (a + b) / 2 + beita * (b - a)
+        f1 = funJ(x1)
+        f2 = funJ(x2)
+        if f1 >= f2:
+            t1 = x1
+            e1 = abs((b - x1) / (a0 - b0))
+            optim = x2
+            minf = f2
+        else:
+            t2 = x2
+            e1 = abs((b - x1) / (a0 - b0))
+            optim = x1
+            minf = f1
+        e2 = abs(f2 - f1) / (1 + abs(f1))
+        if e1 <= eer1 and e2 <= eer2:
+            flag = False
+    print(f"t={optim:.8f},minf={minf:.8f},n={n}")
+    end_time = time.time()
+    print("程序运行计时", end_time - start_time)
     x = np.linspace(x_start, x_end, 100)
-    y = funJ(x, a, b, c, d, e)
+    y = funJ(x)
     plt.figure(num="目标函数与优化区间变量关系图")
     plt.plot(x, y, lw=2, c="b")
     plt.grid()
@@ -56,13 +67,13 @@ def Exhau(funJ, N, Pc, start, alfai):
     plt.annotate(
         "最优点",
         xy=(optim, minf),
-        xytext=(optim + 1, minf + 0.5),
+        xytext=(optim + 0.1, minf + 0.15),
         arrowprops=dict(arrowstyle="->", color="r", linewidth=2),
     )
     str1 = "x=" + str(optim)
     str2 = "最优值=" + str(minf)
-    plt.text(optim - 0.5, minf + 0.25, str1)
-    plt.text(optim - 0.5, minf + 0.15, str2)
+    plt.text(optim - 0.05, minf + 0.10, str1)
+    plt.text(optim - 0.05, minf + 0.05, str2)
     plt.show()
 
 
@@ -70,16 +81,16 @@ def Exhau(funJ, N, Pc, start, alfai):
 def fast_scan(start, alfai):
     n = 1
     t1 = start
-    f1 = funJ(t1, a, b, c, d, e)
+    f1 = funJ(t1)
     t2 = t1 + alfai * 2 ** (n - 1)
-    f2 = funJ(t2, a, b, c, d, e)
+    f2 = funJ(t2)
     if f1 < f2:
         alfai = -alfai
         t2 = t1 + alfai * 2 ** (n - 1)
     while n < 1000:
         n = n + 1
         t3 = t2 + alfai * 2 ** (n - 1)
-        f3 = funJ(t3, a, b, c, d, e)
+        f3 = funJ(t3)
         if f2 < f3:
             break
         else:
@@ -90,14 +101,21 @@ def fast_scan(start, alfai):
     return [t1, t3]
 
 
-def funJ(x, a, b, c, d, e):  ##fitness
+def funJ(x):  ##fitness
+    a1, b1, c1, d1 = 2, 6, 5, 7
+    f = ((x + a1) ** 2 + b1**2) ** 0.5 + ((-x + c1) ** 2 + d1**2) ** 0.5
+    return f  # 求最大变成求最小，前面加负号，默认求最小
+
+
+""" def funJ(x):  ##fitness
+    a, b, c, d, e = 1, 3, 5, 7, 2
     f = (
         (x**2 + a**2) ** 0.5
         + ((x + b) ** 2 + c**2) ** 0.5
         + ((x + d) ** 2 + e**2) ** 0.5
     )
-    return f  # 求最大变成求最小，前面加负号，默认求最小
+    return f  # 求最大变成求最小，前 """
 
 
 # if __name__ == '__main__':
-Exhau(funJ, 100, 10e-6, 1, 0.1)
+Bisexhau(funJ, 0.001, 10e-5, 10e-5, -1, 0.1)
